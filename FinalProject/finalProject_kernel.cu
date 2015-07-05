34e99e160604d38ed03862432fb0caa07a104366
yuval.bercovich@gmail.com <yuval.bercovich@gmail.com>
2015-07-05 23:18:31 +0300

    kernel5 - no modulo

--- FinalProject/finalProject_kernel.cu
+++ FinalProject/finalProject_kernel.cu
@@ -28,6 +28,78 @@ struct SharedMemory
 	}
 };
 
+// no % operator
+__global__ void kernel5(char* lifeData, int worldWidth, int worldHeight, char* resultLifeData)
+{
+	int worldSize = worldWidth * worldHeight;
+	int colors = 16;
+
+	char *sdata = SharedMemory();
+
+	for (int cellId = __mul24(blockIdx.x, blockDim.x) + threadIdx.x;
+		cellId < worldSize;
+		cellId += blockDim.x * gridDim.x)
+	{
+		//int x = cellId % worldWidth;									// x=0
+		int x = cellId & (worldWidth-1);									// x=0
+		int yAbs = cellId - x;											// yabs = 0
+		//int xLeft = (x + worldWidth - 1) % worldWidth;					// xleft=3
+		int xLeft = (x + worldWidth - 1) & (worldWidth-1);					// xleft=3
+		//int xRight = (x + 1) % worldWidth;								// xright=1
+		int xRight = (x + 1) & (worldWidth-1);								// xright=1
+		//int yAbsUp = (yAbs + worldSize - worldWidth) % worldSize;		// yabsup=12
+		int yAbsUp = (yAbs + worldSize - worldWidth) & (worldSize-1);		// yabsup=12
+		//int yAbsDown = (yAbs + worldWidth) % worldSize;					// yabsdown=4
+		int yAbsDown = (yAbs + worldWidth) & (worldSize-1);					// yabsdown=4
+
+		// load left neighbors to SM
+		sdata[threadIdx.x * 3 + 0] = lifeData[xLeft + yAbsUp];
+		sdata[threadIdx.x * 3 + 1] = lifeData[xLeft + yAbs];
+		sdata[threadIdx.x * 3 + 2] = lifeData[xLeft + yAbsDown];
+
+		// if last thread - load 3 from current col and 3 from right
+		if (threadIdx.x == blockDim.x - 1)
+		{
+			sdata[threadIdx.x * 3 + 3] = lifeData[x + yAbsUp];
+			sdata[threadIdx.x * 3 + 4] = lifeData[x + yAbs];
+			sdata[threadIdx.x * 3 + 5] = lifeData[x + yAbsDown];
+
+			sdata[threadIdx.x * 3 + 6] = lifeData[xRight + yAbsUp];
+			sdata[threadIdx.x * 3 + 7] = lifeData[xRight + yAbs];
+			sdata[threadIdx.x * 3 + 8] = lifeData[xRight + yAbsDown];
+		}
+
+		__syncthreads();
+
+		// now we are ready to work.
+
+		// go to IF, and check neighbors in SM, and output to global memory.
+
+		int currCellLocInSData = 4 + threadIdx.x * 3;
+		char currCellColor = sdata[currCellLocInSData];
+		//char nextColor = (currCellColor + 1) % colors;
+		char nextColor = (currCellColor + 1) & (colors-1);
+
+		if ((sdata[currCellLocInSData - 4] == nextColor) ||
+			(sdata[currCellLocInSData - 3] == nextColor) ||
+			(sdata[currCellLocInSData - 2] == nextColor) ||
+			(sdata[currCellLocInSData - 1] == nextColor) ||
+			(sdata[currCellLocInSData + 1] == nextColor) ||
+			(sdata[currCellLocInSData + 2] == nextColor) ||
+			(sdata[currCellLocInSData + 3] == nextColor) ||
+			(sdata[currCellLocInSData + 4] == nextColor))
+		{
+			resultLifeData[x + yAbs] = nextColor;
+		}
+		else
+		{
+			resultLifeData[x + yAbs] = currCellColor;
+		}
+
+	}
+}
+
+
 // shared memory + less registers
 __global__ void kernel4(char* lifeData, int worldWidth, int worldHeight, char* resultLifeData)
 {
@@ -316,7 +388,7 @@ void  reduce(int boardHeight, int boardWidth, int numThreads, int numBlocks, cha
 	for (size_t i = 0; i < epochs; i++) {
 		cudaDeviceSynchronize();
 		//kernel3 << <numBlocks, numThreads, numThreads*3+6 >> >(*d_idata, boardHeight, boardWidth, *d_odata);
-		kernel4 << <numBlocks, numThreads >> >(*d_idata, boardHeight, boardWidth, *d_odata);
+		kernel5 << <numBlocks, numThreads , 1024*3+6>> >(*d_idata, boardHeight, boardWidth, *d_odata);
 		std::swap(*d_idata, *d_odata);
 	}
 	checkCudaErrors(cudaDeviceSynchronize());
